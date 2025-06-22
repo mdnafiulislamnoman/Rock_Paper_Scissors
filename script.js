264c396c948b71a67d54a27dc5daaa8c8e386ff8
mdnafiulislamnoman <software.engr.nafiul@gmail.com>
2025-06-22 13:34:41 +0600

    I created a function humanChoice. Which will recieve a user prompt of his choice among rock, paper & scissor. I also keep them case insensitive to compare with computers choice.

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-function getComputerChoice (){
+function getComputerChoice() {
     let C_Choice = Math.random() * 100;
-    
-    if (C_Choice <= 35){
-        return "Rock";
-    } else if (C_Choice > 35 && C_Choice <= 70){
-        return "Paper";
-    } else if (C_Choice > 70 && C_Choice <= 100){
-        return "Scissor";
+
+    if (C_Choice <= 35) {
+        let ret1 = "rock";
+        return ret1;
+    } else if (C_Choice > 35 && C_Choice <= 70) {
+        let ret2 = "paper";
+        return ret2;
+    } else if (C_Choice > 70 && C_Choice <= 100) {
+        let ret3 = "scissor";
+        return ret3;
+    }
+}
+
+function humanChoice() {
+    let Str = prompt("Enter your choice:");
+
+    if(Str !== null){
+        Str = Str.toLowerCase();
     }
 }
