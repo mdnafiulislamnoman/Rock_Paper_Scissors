48cb191b7a982ce153a9d3cf7546b25943a8dec4
mdnafiulislamnoman <software.engr.nafiul@gmail.com>
2025-06-22 14:20:27 +0600

    Created a function named PlayRound Where comparing results with computers choice and selecting the winner. With little bit of correction on getComputerChoice funtion.

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ function getComputerChoice() {
     let C_Choice = Math.random() * 100;
 
     if (C_Choice <= 35) {
-        let ret1 = "rock";
-        return ret1;
+        let ret = "rock";
+        return ret;
     } else if (C_Choice > 35 && C_Choice <= 70) {
-        let ret2 = "paper";
-        return ret2;
+        let ret = "paper";
+        return ret;
     } else if (C_Choice > 70 && C_Choice <= 100) {
-        let ret3 = "scissor";
-        return ret3;
+        let ret = "scissor";
+        return ret;
     }
+    return ret;
 }
 
 function humanChoice() {
@@ -19,4 +20,24 @@ function humanChoice() {
     if(Str !== null){
         Str = Str.toLowerCase();
     }
+    return Str;
 }
+
+let HumanScore = 0;
+let ComputerScore = 0;
+
+function PlayRound (HumanChoice,ComputerChoice){
+    if (HumanChoice === ComputerChoice){
+        console.log("Tie!!");
+    } else if ((HumanChoice === "rock" && ComputerChoice === "paper") || (HumanChoice === "scissor") && (ComputerChoice === "rock")){
+        console.log("Computer Wins!!");
+        ComputerScore ++;
+    } else {
+        console.log("Human wins!!")
+        HumanScore ++;
+    }
+}
+
+let Str = humanChoice ();
+let ret = getComputerChoice ();
+PlayRound(Str,ret);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+
+    } else if (C_Choice > 35 && C_Choice <= 70) {
+        let ret = "paper";
+        return ret;
+    } else if 
